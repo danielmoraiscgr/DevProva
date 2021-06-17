d81611aaf853e03702a18e4402512ce8d475e0f0
Daniel Morais Boges <daniel.morais@bumlai.com.br>
2021-06-17 13:05:22 -0400

    Prova Desenvolvimento Colaborativo

---
+++ CalculoBhaskara.cs
@@ -0,0 +1,30 @@
+﻿using System;
+namespace DevProva
+{
+    public class CalculoBhaskara
+    {
+        public double ResultadoCalculoBhaskara(int a, int b,int c)
+        {
+
+            double delta = (b * b) - (4 * a * c);
+
+            double raiz = Math.Sqrt(delta);           
+
+            if (delta > 0)
+            {
+                var x1 = ((-(b)) + raiz) / (2 * a);
+
+                var x2 = ((-(b)) - raiz) / (2 * a);
+
+                return Math.Max(x1, x2);
+            }
+            else if (delta == 0)
+            {
+                return 0;
+            }
+            else
+                return -1;            
+
+        }
+    }
+}
---
+++ Controllers/CalculoBhaskaraController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace DevProva.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class CalculoBhaskaraController : ControllerBase
+    {
+
+        [HttpGet]
+        public double Get()
+        {
+            CalculoBhaskara calculo = new CalculoBhaskara();
+            return calculo.ResultadoCalculoBhaskara(1, -5, 6); 
+        }
+        
+
+
+    }
+}
